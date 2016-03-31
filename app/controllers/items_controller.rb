c41779a8f7be31cd5a5185be5b43a8c38ded1ada
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-01 03:15:05 +0600

    Add edit, update & destroy actions, add before_actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :find_item,      only: [:show, :edit, :update, :destroy]
+  before_action :check_if_admin, only: [:edit, :update, :new, :create, :destroy]
 
   def index
     @items = Item.all
   end
 
-  # /items/1 GET
+  # /items/:id GET
   def show
-    unless @item = Item.where(id: params[:id]).first
+    unless @item
       render text: 'Page not found', status: 404
     end
   end
@@ -16,29 +18,47 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  # /items/edit/1 GET
+  # /items/edit/:id GET
   def edit; end
 
   # /items POST
   def create
     @item = Item.create(item_params)
     if @item.errors.empty?
-      redirect_to item_path(@item)
+      redirect_to item_path(@item) # /items/:id
     else
       render :new
     end
   end
 
-  # /items/1 PUT, PATCH
-  def update; end
+  # /items/:id PUT, PATCH
+  def update
+    @item.update_attributes(item_params)
+    if @item.errors.empty?
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
 
-  # /items/1 DELETE
-  def destroy; end
+  # /items/:id DELETE
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
 
   private
 
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:price, :name, :real, :weight, :description)
+  end
+
+  def check_if_admin
+    render text: 'Access denied', status: 404 unless params[:admin]
   end
 
 end
