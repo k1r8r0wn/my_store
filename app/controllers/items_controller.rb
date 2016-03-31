d3a250792c847151b2ca91c5c12b4a776f4c1fe1
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-01 02:24:26 +0600

    Add more actions to items controller, add views, change routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,38 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    render text: @items.map {|i| "#{i.name}: #{i.price}"}.join('<br/>')
   end
 
+  # /items/1 GET
+  def show
+    unless @item = Item.where(id: params[:id]).first
+      render text: 'Page not found', status: 404
+    end
+  end
+
+  # /items/new GET
+  def new
+    @item = Item.new
+  end
+
+  # /items/edit/1 GET
+  def edit; end
+
+  # /items POST
   def create
     @item = Item.create(item_params)
-    render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+    if @item.errors.empty?
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
+
+  # /items/1 PUT, PATCH
+  def update; end
+
+  # /items/1 DELETE
+  def destroy; end
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,4 @@ class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
 
-  after_initialize { puts 'initialized' } #Item.new; Item.first
-  after_save       { puts 'saved' } #Item.save; Item.create; item.
-  after_create     { puts 'created' }
-  after_update     { puts 'updated' }
-  after_destroy    { puts 'destroyed' } #Item.destroy
-
 end
