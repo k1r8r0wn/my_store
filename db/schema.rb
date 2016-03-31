79479e0ca127794c106cb76f165b808d6c68440b
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-01 04:24:59 +0600

    Generate User & Cart models, add associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331212141) do
+ActiveRecord::Schema.define(version: 20160331221501) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.float    "price"
@@ -22,6 +28,12 @@ ActiveRecord::Schema.define(version: 20160331212141) do
     t.datetime "updated_at",              null: false
     t.string   "description"
     t.integer  "votes_count", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "login"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
