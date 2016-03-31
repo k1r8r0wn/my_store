2789b99bc113108799fb6fcac97d663a39bd3953
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-01 05:49:23 +0600

    Add has_and_belongs_to_many associations to new table
    Create cart_items table

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
 
+  has_and_belongs_to_many :carts
 end
