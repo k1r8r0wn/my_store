7688352e0208fcbc813bc6b354c1a1b6b9828e52
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-01 06:05:59 +0600

    Create new Position model and change associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ActiveRecord::Base
 
   validates :price, numericality: { greater_than: 0, allow_nil: true }
-  validates :name, :description, presence: true
+  validates :name, presence: true
 
-  has_and_belongs_to_many :carts
+  has_many :positions
+  has_many :carts, through: :positions
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331233826) do
+ActiveRecord::Schema.define(version: 20160331235340) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "carts_items", id: false, force: :cascade do |t|
-    t.integer "cart_id"
-    t.integer "item_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -37,6 +32,14 @@ ActiveRecord::Schema.define(version: 20160331233826) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.integer  "cart_id"
+    t.integer  "item_id"
+    t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
