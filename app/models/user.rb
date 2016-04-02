4605370d91dfd382bf5d3c03e021c6757fee2e18
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-02 15:31:53 +0600

    Add device to User model, add views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   has_one  :cart
   has_many :orders
   has_many :comments
+
+  validates_uniqueness_of :email
 
 end
