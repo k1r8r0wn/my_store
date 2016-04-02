352b788004614df9b5c336424cb3b5c33c267619
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-04-02 18:02:15 +0600

    Add helpers to items, change description text_field to text_area

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def urls_to_images(s)
+    s.gsub! /\s((http|https):\/\/.*?\.(png|jpg))(\s|\Z)/,
+            '<p><img src="\1"/></p>'
+    s.html_safe
+  end
+
+  def urls_to_links(s)
+    s.gsub! /\s((http|https):\/\/.*?)\s/,
+            '<a href="\1">\1</a>'
+    s.html_safe
+  end
+
 end
